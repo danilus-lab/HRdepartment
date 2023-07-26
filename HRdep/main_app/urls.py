d43a666916622ca73a1d808ce25cbf085c50d478
Daniil <daniil.novsky@yandex.ru>
2023-07-26 15:12:00 +0300

    staff_list added

--- HRdep/main_app/urls.py
+++ HRdep/main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('dismiss', views.dismiss, name='dismiss'),
-    path('workers', views.workers, name='workers')
+    path('workers', views.workers, name='workers'),
+    path('staff_list', views.staff_list, name='staff_list')
 ]
--- HRdep/main_app/views.py
+++ HRdep/main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Worker
+from .models import Worker, Dep, Stafflist
 
 
 def index(request):
@@ -13,3 +13,8 @@ def dismiss(request):
 def workers(request):
     workers = Worker.objects.all()
     return render(request, "main_app/workers.html", {'title': 'Список сотрудников', 'workers': workers})
+
+
+def staff_list(request):
+    notes = Stafflist.objects.all()
+    return render(request, "main_app/staff_list.html", {'title': 'Штатное расписание', 'notes': notes})
